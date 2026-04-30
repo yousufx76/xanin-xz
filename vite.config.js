e4d1e105f7cc111ad2e3c69ff674ec17084ebd57
xaninstudio-lang <xaninstudio@gmail.com>
2026-04-30 12:52:38 +0600

    add sitemap and vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://xanin-xz.vercel.app',
+      routes: [
+        '/',
+        '/about',
+        '/works',
+        '/cv',
+        '/contact',
+      ]
+    })
+  ],
 })
