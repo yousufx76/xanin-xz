4105066ba4374cf1f0036dae2145c1f1d2333c25
xaninstudio-lang <xaninstudio@gmail.com>
2026-06-05 10:00:53 +0600

    cleanup: remove xena worker and functions

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import Events from './pages/Events'
 import Services from './pages/Services'
 import Premade from './pages/Premade'
 import NotFound from './pages/NotFound'
-import XenaChat from './components/XenaChat'
 
 
 
@@ -49,7 +48,6 @@ function App() {
             <Route path="*" element={<NotFound />} />
 
           </Routes>
-          <XenaChat />
           <Footer />
         </>
       )}
